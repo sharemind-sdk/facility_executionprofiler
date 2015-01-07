5b5bb3b4d081ef4fdf8f7192584384b0c0e48a99
Karl Tarbe <karl.tarbe@cyber.ee>
2015-01-07 13:40:42 +0200

    Simplified the facility significantly

--- src/lib.cpp
+++ src/lib.cpp
@@ -13,38 +13,6 @@
 #include <string>
 #include <unordered_map>
 
-
-namespace {
-
-struct Facility: ::SharemindModuleApi0x1Facility {
-    inline Facility(void * const facility, void * const context = nullptr)
-        : ::SharemindModuleApi0x1Facility{facility, context}
-    {}
-    virtual ~Facility() noexcept {}
-};
-using FacilityPointer = ::std::shared_ptr<Facility>;
-
-struct ExecutionProfilerFacility: Facility {
-    inline ExecutionProfilerFacility(::LogHard::Backend & backend)
-        : Facility{new ::sharemind::ExecutionProfiler(::LogHard::Logger(backend))}
-        , executionProfiler{static_cast<SHAREMIND_GCCPR54526::sharemind::ExecutionProfiler *>(
-                     this->facility)}
-    {}
-    ::std::unique_ptr<SHAREMIND_GCCPR54526::sharemind::ExecutionProfiler> executionProfiler;
-};
-
-using FacilityMap = ::std::unordered_map<SHAREMIND_GCCPR54526::std::string,
-                                         FacilityPointer>;
-
-struct ModuleData {
-    inline ModuleData(const char* conf) {};
-    FacilityMap moduleFacilities;
-    FacilityMap pdFacilities;
-    FacilityMap pdpiFacilities;
-};
-
-} // anonymous namespace
-
 extern "C" {
 
 SHAREMIND_FACILITY_MODULE_API_MODULE_INFO("Profiler", 1u, 1u);
@@ -53,7 +21,14 @@ SHAREMIND_FACILITY_MODULE_API_0x1_INITIALIZER(c,errorStr);
 SHAREMIND_FACILITY_MODULE_API_0x1_INITIALIZER(c,errorStr) {
     assert(c);
     try {
-        c->moduleHandle = new ModuleData{c->conf};
+        const SharemindModuleApi0x1Facility * logger =
+            c->findModuleFacility(c, c->conf);
+
+        if (!logger || !logger->facility)
+            return ::SHAREMIND_FACILITY_MODULE_API_0x1_MODULE_ERROR;
+
+        ::LogHard::Backend * const b = static_cast<::LogHard::Backend *>(logger->facility);
+        c->moduleHandle = new sharemind::ExecutionProfiler{*b};
         return ::SHAREMIND_FACILITY_MODULE_API_0x1_OK;
     } catch (::std::bad_alloc const &) {
         return ::SHAREMIND_FACILITY_MODULE_API_0x1_OUT_OF_MEMORY;
@@ -66,7 +41,7 @@ SHAREMIND_FACILITY_MODULE_API_0x1_DEINITIALIZER(c);
 SHAREMIND_FACILITY_MODULE_API_0x1_DEINITIALIZER(c) {
     assert(c);
     assert(c->moduleHandle);
-    delete static_cast<ModuleData *>(c->moduleHandle);
+    delete static_cast<sharemind::ExecutionProfiler *>(c->moduleHandle);
 }
 
 #define STUFF(name,NAME) \
@@ -75,10 +50,9 @@ SHAREMIND_FACILITY_MODULE_API_0x1_DEINITIALIZER(c) {
     { \
         assert(c); \
         assert(c->moduleHandle); \
-        auto const & map = \
-                static_cast<ModuleData *>(c->moduleHandle)->name ## Facilities;\
-        auto const it = map.find(signature); \
-        return (it == map.cend()) ? nullptr : it->second.get(); \
+        return std::strcmp(signature, "Profiler") != 0 \
+               ? nullptr \
+               : static_cast<SharemindModuleApi0x1Facility *>(c->moduleHandle); \
     }
 STUFF(module,MODULE)
 STUFF(pd,PD)
